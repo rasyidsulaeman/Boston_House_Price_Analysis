6a4f75cfcfb6d89a494f4cfc7334e5d142e80633
rasyidsulaeman <rasyid.sulaeman@sci.ui.ac.id>
2024-05-08 20:04:42 +0700

    update boston app

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import shap
 from sklearn.ensemble import RandomForestRegressor
 from streamlit_shap import st_shap
 
+
 # give description and title of the project
 st.write("""
 # Boston House Price Prediction
@@ -21,10 +22,9 @@ st.divider()
 # use sidebar to layout-ing the website
 sidebar = ('Exploratory Data Analysis', 'Machine Learning Boston House Prediction')
 with st.sidebar:
+    st.markdown('##### Author : Rasyid Sulaeman')
     st.header('Select Data Processess')
-    radio = st.radio('Select', options=sidebar, label_visibility='hidden')
-    if radio == sidebar[1]:
-        st.header('Specify input parameters')
+    radio = st.radio('Modes', options=sidebar)
 
 # load dataset
 path = 'dataset/boston.csv'
@@ -34,36 +34,6 @@ df = df.astype('float64')
 
 X = df.drop(columns='MEDV')
 Y = df['MEDV']
-
-def user_input_features():
-    CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-    ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-    CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-    NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-    RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-    AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-    DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-    RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-    TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-    BLACK = st.sidebar.slider('BLACK', X.BLACK.min(), X.BLACK.max(), X.BLACK.mean())
-    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-    data = {'CRIM': CRIM,
-            'ZN': ZN,
-            'INDUS': INDUS,
-            'CHAS': CHAS,
-            'NOX': NOX,
-            'RM': RM,
-            'AGE': AGE,
-            'DIS': DIS,
-            'RAD': RAD,
-            'TAX': TAX,
-            'PTRATIO': PTRATIO,
-            'BLACK': BLACK,
-            'LSTAT': LSTAT}
-    features = pd.DataFrame(data, index=[0])
-    return features
 
 if radio == sidebar[0]:
 
@@ -115,22 +85,59 @@ if radio == sidebar[0]:
 
 elif radio == sidebar[1]:
 
-    df = user_input_features()
-
-    # Print specified input parameters
     st.header('Specified Input parameters')
-    st.write(df)
+    
+    columns = st.columns(3)
+    with columns[0]:
+        CRIM = st.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
+        ZN = st.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
+        INDUS = st.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
+        CHAS = st.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
+        NOX = st.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
+    
+    with columns[1]:
+        RM = st.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
+        AGE = st.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
+        DIS = st.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
+        RAD = st.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
+    
+    with columns[2]:
+        TAX = st.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
+        PTRATIO = st.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
+        BLACK = st.slider('BLACK', X.BLACK.min(), X.BLACK.max(), X.BLACK.mean())
+        LSTAT = st.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
+    
+    data = {'CRIM': CRIM,
+            'ZN': ZN,
+            'INDUS': INDUS,
+            'CHAS': CHAS,
+            'NOX': NOX,
+            'RM': RM,
+            'AGE': AGE,
+            'DIS': DIS,
+            'RAD': RAD,
+            'TAX': TAX,
+            'PTRATIO': PTRATIO,
+            'BLACK': BLACK,
+            'LSTAT': LSTAT}
+    
+    features = pd.DataFrame(data, index=[0])
+   
+    # Print specified input parameters
+    st.write('##### This is your defined parameters')
+    st.write(features)
     st.write('---')
 
     # Build Regression Model
     model = RandomForestRegressor()
     model.fit(X, Y)
-    # Apply Model to Make Prediction
-    prediction = model.predict(df)
 
+    # Apply Model to Make Prediction
+    prediction = model.predict(features)
+
+    # print(prediction.shape)
     st.header('Prediction of MEDV')
-    st.write(prediction)
-    st.write('---')
+    st.metric('Prediction', round(prediction[0],3), label_visibility='hidden')
 
     # # Explaining the model's predictions using SHAP values
     # # https://github.com/slundberg/shap
