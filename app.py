a69b335f85646b30a95df7905ddd9b217e4af375
rasyidsulaeman <rasyid.sulaeman@sci.ui.ac.id>
2024-05-09 12:18:44 +0700

    add hyperparameter tuning

--- app.py
+++ app.py
@@ -2,17 +2,21 @@
 
 import streamlit as st
 from boston import * 
-import time
 import shap
+import numpy as np
+
 from sklearn.ensemble import RandomForestRegressor
-from streamlit_shap import st_shap
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+
+st.set_page_config(page_title='Boston House Price Prediction', layout='wide')
 
 # give description and title of the project
 st.write("""
 # Boston House Price Prediction
          
 A web application to predict and analyze Boston House Price dataset.
-Powered by **streamlit**.
+Powered by `streamlit`.
          
 For more information about the dataset, check [this website](https://www.cs.toronto.edu/~delve/data/boston/bostonDetail.html)
 """)
@@ -22,6 +26,7 @@ st.divider()
 sidebar = ('Exploratory Data Analysis', 'Machine Learning Boston House Prediction')
 with st.sidebar:
     st.markdown('##### Author : Rasyid Sulaeman')
+    st.markdown('##### Check out the repo [here](https://github.com/rasyidsulaeman/Boston_House_Price_Analysis)')
     st.header('Select Data Processess')
     radio = st.radio('Modes', options=sidebar)
 
@@ -71,7 +76,6 @@ if radio == sidebar[0]:
     choose = st.selectbox('Choose following options', options=option)
     with st.spinner('Wait for it . . .'):
         st.write(boston.plots(df, choose))
-        time.sleep(5)
     st.success('Done!')
 
     st.write("""
@@ -84,7 +88,77 @@ if radio == sidebar[0]:
 
 elif radio == sidebar[1]:
 
-    st.header('Specified Input parameters')
+    st.write('## Machine Learning - Random Forest Regression Model')
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    st.markdown('A model is being built to predict the following **Y** variable:')
+    st.info(Y.name)
+
+    st.write('#### Tune the hyperparameter')
+
+    parameter_columns = st.columns(2)
+
+    with parameter_columns[0]:
+        n_estimators = st.slider('Number of estimators', 0, 500, 100)
+        criterion = st.select_slider('Performance measure (criterion)', options=['squared_error', 'absolute_error'])
+        max_features = st.slider('Max features', 1, 50, 1)
+    with parameter_columns[1]:
+        max_depth = st.slider('The maximum depth of the tree', 1, 200, 20) 
+        min_samples_split = st.slider('Minimum number of samples required to split an internal node (min_samples_split)', 1, 10, 2, 1)
+        min_samples_leaf = st.slider('Minimum number of samples required to be at a leaf node (min_samples_leaf)', 1, 10, 2, 1)
+
+    forest_regression = RandomForestRegressor(n_estimators=n_estimators,
+                                              criterion=criterion,
+                                              max_features=max_features,
+                                              max_depth=max_depth,
+                                              min_samples_split=min_samples_split,
+                                              min_samples_leaf=min_samples_leaf,
+                                              random_state=42, n_jobs=-1)
+    forest_regression.fit(X_train, y_train)
+
+    y_pred = forest_regression.predict(X_test)
+
+    r2 = r2_score(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    
+    st.write('Coefficient of determination ($R^2$):')
+    st.info(r2)
+
+    st.write('Root mean squared error:')
+    st.info(rmse)
+
+    st.write('Model parameters')
+    st.write(forest_regression.get_params(deep=True))
+
+    
+    reg_df = pd.DataFrame({'Observed' : y_test, 
+                           'Predicted' : y_pred})
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    scatter_plot = sns.regplot(x='Observed', y='Predicted', data=reg_df, ax=ax)
+    text = f'R-squared: {r2:.2f}\nRMSE: {rmse:.2f}'
+    ax.text(0.05, 0.9, text, transform=ax.transAxes, fontsize=12)
+    scatter_plot.set_xlabel('Observed')
+    scatter_plot.set_ylabel('Predicted')
+
+    st.write('#### Predicted vs Observed Linear Relationship')
+    st.pyplot(fig)
+    
+    # Explaining the model's predictions using SHAP values
+    explainer = shap.Explainer(forest_regression, X_test)
+    shap_values = explainer(X_test)
+
+    st.write('#### Feature Importance') 
+    st.markdown('Visualized feature contribution using `shap` package')
+    fig, ax = plt.subplots(figsize=(6, 6), dpi=150)
+    shap.plots.waterfall(shap_values[0], max_display=15, show=False)
+    st.pyplot(fig)
+
+    st.write('---')
+    st.write('## Boston Price Prediction')
+
+    st.write('#### Specified Input parameters')
     
     columns = st.columns(3)
     with columns[0]:
@@ -127,29 +201,15 @@ elif radio == sidebar[1]:
     st.write(features)
     st.write('---')
 
-    # Build Regression Model
-    model = RandomForestRegressor()
-    model.fit(X, Y)
-
     # Apply Model to Make Prediction
-    prediction = model.predict(features)
+    prediction = forest_regression.predict(features)
 
     # print(prediction.shape)
     st.header('Prediction of MEDV')
-    st.metric('Prediction', round(prediction[0],3), label_visibility='hidden')
+    st.info(round(prediction[0],4))
 
-    # # Explaining the model's predictions using SHAP values
-    # # https://github.com/slundberg/shap
-    # explainer = shap.TreeExplainer(model)
-    # shap_values = explainer.shap_values(X)
+  
 
-    # st.header('Feature Importance')
-    # plt.title('Feature importance based on SHAP values')
-    # st_shap(shap.summary_plot(shap_values, X))
-
-   
-
-
-
+    
 
 
